f68c5536c7cd6139149cab3c3fc75e80c9724d97
Isaac Sanchez <jelito@gmail.com>
2022-07-30 16:39:55 +0100

    🌬 Update Tailwind config - Exclude unused themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,6 @@ module.exports = {
   },
   plugins: [require('@tailwindcss/typography'), require('daisyui')],
   daisyui: {
-    themes: ['cupcake', 'dark']
+    themes: ['light', 'cupcake', 'dark']
   }
 };
